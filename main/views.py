b8e9181cfe82749b8a890af6075e1de7cf5e6f96
Andrey <fox451@yandex.ru>
2020-02-08 20:03:19 +0700

    Add auth, profile

---
+++ main/views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.http import Http404, HttpResponse
+from django.shortcuts import render
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
+
+
+def other_page(request, page):
+    try:
+        template = get_template('main/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
+
+
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
+
+
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
