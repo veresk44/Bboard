850bd237538b96e38d551d8fc42355e1c16099ca
Andrey <fox451@yandex.ru>
2020-02-08 20:52:32 +0700

    Add change user data

--- main/views.py
+++ main/views.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView
+
+from main.models import AdvUser
+from .forms import ChangeUserInfoForm
 
 
 def other_page(request, page):
@@ -28,7 +34,23 @@ class BBLogoutView(LoginRequiredMixin, LogoutView):
     template_name = 'main/logout.html'
 
 
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin,
+                         UpdateView):
+    model = AdvUser
+    template_name = 'main/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Личные данные пользователя изменены'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
+
+
 def index(request):
     return render(request, 'main/index.html')
-
-
