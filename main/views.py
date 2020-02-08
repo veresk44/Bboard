4f5b92694ecf9d1080b00be8eb49bb0d83b4f1bd
Andrey <fox451@yandex.ru>
2020-02-08 21:12:53 +0700

    Add change password

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import Http404, HttpResponse
 from django.shortcuts import render, get_object_or_404
@@ -50,6 +50,13 @@ class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin,
         if not queryset:
             queryset = self.get_queryset()
         return get_object_or_404(queryset, pk=self.user_id)
+
+
+class BBPasswordChangeView(SuccessMessageMixin, LoginRequiredMixin,
+                           PasswordChangeView):
+    template_name = 'main/password_change.html'
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Пароль пользователя изменен'
 
 
 def index(request):
